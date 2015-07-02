01c80af15c13c0430a03ee13a564a46f927eb59c
lovedboy <lovedboy.tk@qq.com>
2015-07-02 13:04:01 +0800

    add optparse

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ import tornado.web
 import sys
 import select
 import os
+from optparse import OptionParser
 
-PORT = int(sys.argv[1])
 LISTENERS = []
 
-
-def check_file():
+def read_file():
 
     while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
         line = sys.stdin.readline()
@@ -39,35 +38,48 @@ class TailHandler(tornado.websocket.WebSocketHandler):
             LISTENERS.remove(self)
         except:
             pass
- 
- 
 
 class IndexHandler(tornado.web.RequestHandler):
 
     def get(self):
-	self.render('index.html', host=self.request.host)
+        self.render('index.html', host=self.request.host)
  
 site_root = os.path.dirname(__file__)
 settings = {
     "static_path":os.path.join(site_root,'static'),
     "template_path":os.path.join(site_root,'templates'),
-    "debug":True,
+    "debug":False,
     }
 
 application = tornado.web.Application([
     (r'/tail/', TailHandler),
     (r'/', IndexHandler),
 ], **settings)
- 
-if __name__ == '__main__':
+
+def server(host, port):
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(PORT)
- 
-    tailed_callback = tornado.ioloop.PeriodicCallback(check_file, 500)
+    http_server.listen(port=port, address=host)
+
+    tailed_callback = tornado.ioloop.PeriodicCallback(read_file, 500)
     tailed_callback.start()
- 
+
     io_loop = tornado.ioloop.IOLoop.instance()
     try:
         io_loop.start()
     except SystemExit, KeyboardInterrupt:
         io_loop.stop()
+
+def main():
+    parse = OptionParser(version='pytaillog 0.1')
+    parse.add_option('-H','--host', help="listen host, default 0.0.0.0",default='0.0.0.0')
+    parse.add_option('-P','--port', help="listen port, default 8080",default=8080, type="int")
+    (options,args) = parse.parse_args()
+
+    host = options.host
+    port = options.port
+    server(host, port)
+
+if __name__ == '__main__':
+
+    main()
+
